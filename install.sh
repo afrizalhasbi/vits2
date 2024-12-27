6c358811a874d675f285ae9b22915fcec1bc2f22
Afrizal Hasbi Azizy <afrizal.azizy@gmail.com>
2024-12-27 10:59:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ pip install pydantic==1.10.4
 pip install ruamel.yaml
 pip install git+https://github.com/openai/whisper.git
 
+wget https://huggingface.co/spaces/Plachta/VITS-Umamusume-voice-synthesizer/resolve/main/pretrained_models/D_trilingual.pth -O ./pretrained_models/D_0.pth
+wget https://huggingface.co/spaces/Plachta/VITS-Umamusume-voice-synthesizer/resolve/main/pretrained_models/G_trilingual.pth -O ./pretrained_models/G_0.pth
+wget https://huggingface.co/spaces/Plachta/VITS-Umamusume-voice-synthesizer/resolve/main/configs/uma_trilingual.json -O ./configs/finetune_speaker.json
+
+wget https://huggingface.co/datasets/Plachta/sampled_audio4ft/resolve/main/sampled_audio4ft_v2.zip
+unzip sampled_audio4ft_v2.zip
+rm -rf sampled_audio4ft_v2.zip
+
 # build monotonic align
 cd monotonic_align/
 mkdir monotonic_align

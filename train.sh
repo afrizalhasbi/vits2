cf4a9b56252c610ee7e276de3f0043d53ee2552d
Afrizal Hasbi Azizy <afrizal.azizy@gmail.com>
2024-12-27 14:36:30 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 python scripts/resample.py
-python preprocess_v2.py --add_auxiliary_data True --languages
+python preprocess_v2.py --add_auxiliary_data True --languages "CJE"
 python finetune_speaker_v2.py -m ./OUTPUT_MODEL --max_epochs "10" --drop_speaker_embed True
